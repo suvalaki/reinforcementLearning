6f98bed9497c9ff3578db93649b17233e59bbc5d
david <david.ceglia.diaz@gmail.com>
2023-01-20 19:33:00 +1100

    Added constant step_size TMP

--- reinfocement/src/policy/step_size.hpp
+++ reinfocement/src/policy/step_size.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <random>
+#include <type_traits>
 #include <xtensor/xfixed.hpp>
 #include <xtensor/xrandom.hpp>
 
@@ -18,6 +19,25 @@ concept step_size_taker = requires(T t) {
     } -> std::same_as<typename T::PrecisionType>;
 };
 
+template <auto X> inline constexpr bool admissible_is_between() {
+  return 0.0F < X and 1.0F > X;
+};
+
+template <auto STEP_S>
+concept allowed_step_size = admissible_is_between<STEP_S>() &&
+    std::is_floating_point<decltype(STEP_S)>::value;
+
+template <isStateActionValue VALUE_T, auto STEP_S>
+requires allowed_step_size<STEP_S>
+struct constant_step_size_taker {
+  using StateValueType = VALUE_T;
+  using PrecisionType = typename StateValueType::PrecisionType;
+  static typename StateValueType::PrecisionType
+  getStepSize(const StateValueType &value) {
+    return STEP_S;
+  }
+};
+
 template <isStateActionValue VALUE_T> struct weighted_average_step_size_taker {
   using StateValueType = VALUE_T;
   using PrecisionType = typename StateValueType::PrecisionType;
@@ -26,4 +46,5 @@ template <isStateActionValue VALUE_T> struct weighted_average_step_size_taker {
     return 1.0 / (value.step + 1);
   }
 };
+
 } // namespace policy
