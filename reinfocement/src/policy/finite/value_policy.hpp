d854e8fe537415da7639e3bd45263958b70cd52a
david <david.ceglia.diaz@gmail.com>
2023-03-04 13:41:13 +1100

    Add filter to argmax method such that we only look at actions available from each state

--- reinfocement/src/policy/finite/value_policy.hpp
+++ reinfocement/src/policy/finite/value_policy.hpp
@@ -6,6 +6,7 @@
 #include <vector>
 
 #include "environment.hpp"
+#include "policy/combination_policy.hpp"
 #include "policy/finite/policy.hpp"
 #include "policy/objectives/finite_value.hpp"
 #include "policy/objectives/finite_value_function.hpp"
@@ -30,19 +31,25 @@ struct FinitePolicyValueFunctionMixin
   using BaseType = BASE_VALUEFUNCTION_POLICY_T;
   SETUP_TYPES_FROM_NESTED_ENVIRON(SINGLE_ARG(BASE_VALUEFUNCTION_POLICY_T::EnvironmentType));
   using ValueFunctionType = objectives::FiniteValueFunction<BASE_VALUEFUNCTION_POLICY_T, INCREMENTAL_STEPSIZE_T>;
+  using ValueFunctionBaseType = typename ValueFunctionType::ValueFunctionBaseType;
   using KeyMaker = typename ValueFunctionType::KeyMaker;
   using ValueType = typename ValueFunctionType::ValueType;
   using StepSizeTaker = typename ValueFunctionType::StepSizeTaker;
 
   ActionSpace getArgmaxAction(const EnvironmentType &e, const StateType &s) const override;
+  using ValueFunctionType::initialize;
 };
 
 template <isPolicyValueFunctionMixin BASE_VALUEFUNCTION_POLICY_T, objectives::isStepSizeTaker INCREMENTAL_STEPSIZE_T>
 typename FVT::ActionSpace FVT::getArgmaxAction(const EnvironmentType &e, const StateType &s) const {
 
   auto action = ActionSpace{}; // start with a random action so we at least have one that is permissible
-  auto maxIdx = std::max_element(
-      this->begin(), this->end(), [](const auto &p1, const auto &p2) { return p1.second < p2.second; });
+  auto availableActions = e.getReachableActions(s);
+  auto maxIdx = std::max_element(this->begin(), this->end(), [&e, &availableActions](const auto &p1, const auto &p2) {
+    if (availableActions.find(KeyMaker::get_action_from_key(e, p2.first)) == availableActions.end())
+      return p1.second < p2.second;
+    return false;
+  });
   if (maxIdx != this->end()) {
     action = KeyMaker::get_action_from_key(e, maxIdx->first);
   }
